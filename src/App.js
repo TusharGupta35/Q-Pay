5acf65def9829972026717cb18a614dbefe67823
Tushar Gupta <typhoon.g35@gmail.com>
2021-10-24 11:22:17 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import Navbar from './Navbar.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Q-pay</h1>
+      <Navbar />
     </div>
   );
 }
