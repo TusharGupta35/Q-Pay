3407c70e915f440a2737529e9473cd2e25faa911
Tushar Gupta <typhoon.g35@gmail.com>
2022-07-23 21:16:58 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import Contact from './Contact';
 import Footer from './Footer';
 import Navbar from './Navbar.js';
 import Services from './Services.jsx';
-import { Route, Switch } from "react-router";
 import Home from './Home';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Switch>
-        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/home" component={Home}></Route>
         <Route path="/about" component={Aboutus}></Route>
         <Route path="/contact" component={Contact}></Route>
         <Route path="/service" component={Services}></Route>
-        <Route component={Home}></Route>
+        <Redirect to='/home' />
       </Switch>
       <Footer />
     </div>
--- src/Navbar.js
+++ src/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
                     <div className={`collapse navbar-collapse ${show ? "show" : ""}`}>
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+                                <NavLink className="nav-link" to="/home">Home</NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink className="nav-link" to="/service">Services</NavLink>
